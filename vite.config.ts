6bf16550d7b7f7cdd90d1e169a7b2dbc3ef4bcd3
Vladislav <vladislav.androsovich@gmail.com>
2025-01-24 01:43:37 +0200

    Setup vite build

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,31 @@
+import path from 'path';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import tsConfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tsConfigPaths(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'use-storage-hooks',
+      fileName: (format) => `use-storage-hooks.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
 });
